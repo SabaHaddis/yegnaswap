bacd5be1149db0c42db92d51e144aec9e34386f3
Saba Haddis <sabahaddissolomon@gmail.com>
2025-06-09 15:42:59 +0200

    Add html, css for FAQ section

--- scripts.js
+++ scripts.js
@@ -30,3 +30,5 @@ let count = localStorage.getItem('visitCount') || 0;
 count++;
 localStorage.setItem('visitCount', count);
 document.getElementById('visitorCount').innerText = `You've visited this site ${count} times.`;
+
+
